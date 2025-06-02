856f0ef3441fc80f7e242901229a1a47da6dd8e0
Zoibur <147055532+Zoibur@users.noreply.github.com>
2025-06-03 01:59:05 +0200

    Update KeyTemp.cs

--- Assets/Scipts/KeyTemp.cs
+++ Assets/Scipts/KeyTemp.cs
@@ -2,7 +2,5 @@ using UnityEngine;
 
 public class KeyTemp : MonoBehaviour
 {
-    //AIzaSyD71VmXy1GIJzEvXYDfFFUuPQF9Zt2eWDM MapKey
-    
-    //AIzaSyAXvP8nCfEqccs7uvS72RtZyHeR_SfPt3Q APIkey
+   
 }
